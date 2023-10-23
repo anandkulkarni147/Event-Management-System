b5d40465186d3600cf03053fa4962b44259d2eba
Anand Kulkarni <kulkarnianand147@gmail.com>
2023-10-23 12:15:42 -0700

    set up environment

---
+++ src/test/java/com/chorddht/ChordDhtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chorddht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordDhtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
