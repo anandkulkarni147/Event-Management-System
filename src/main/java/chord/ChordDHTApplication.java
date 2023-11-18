c74ad21f57974ef90ee2a1ea22dd9416c2361ec1
Anand Kulkarni <kulkarnianand147@gmail.com>
2023-11-17 20:12:43 -0800

    component scan

--- src/main/java/chord/ChordDHTApplication.java
+++ src/main/java/chord/ChordDHTApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {"chord", event})
 @ComponentScan(basePackages = {"UI", "chord"})
 public class ChordDHTApplication {
 
