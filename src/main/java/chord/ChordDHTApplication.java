151d55ae5248dc68bfbec1ec81c446ee7d5a19ad
Anand Kulkarni <kulkarnianand147@gmail.com>
2023-11-17 20:21:18 -0800

    fixed bean not found

--- src/main/java/chord/ChordDHTApplication.java
+++ src/main/java/chord/ChordDHTApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"event"})
+@ComponentScan(basePackages = {"chord", "event"})
 public class ChordDHTApplication {
 
     public static void main(String[] args) {
