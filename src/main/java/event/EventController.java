4e0add47bd80f6aa8e3273896c0ef3889b6495d3
Anand Kulkarni <kulkarnianand147@gmail.com>
2023-11-30 13:43:45 -0800

    fixed sending emails

--- src/main/java/event/EventController.java
+++ src/main/java/event/EventController.java
@@ -7,7 +7,6 @@ import java.util.TreeMap;
 import javax.servlet.http.HttpSession;
 
 import email.KafkaProducerService;
-import email.ScheduleEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
