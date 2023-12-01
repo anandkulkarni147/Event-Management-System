e2d5128bfe554dc272236aec5d295584daf6914e
Anand Kulkarni <kulkarnianand147@gmail.com>
2023-11-30 20:13:46 -0800

    final

--- src/main/java/email/ScheduleEmailService.java
+++ src/main/java/email/ScheduleEmailService.java
@@ -12,16 +12,26 @@ public class ScheduleEmailService {
     private KafkaConsumerService kafkaConsumerService;
 
     // Schedule the subscribed email sending task at every 60 seconds
-    @Scheduled(fixedRate = 30000)
+    @Scheduled(fixedRate = 60000)
     public void sendSubscribedEmail(Event event) {
-        System.out.println("consumer called");
         kafkaConsumerService.processEvent(event);
     }
 
     // Schedule the email sending task at 6 AM on the event date and one day before
     @Scheduled(cron = "0 0 6 * * ?")
     public void sendScheduledEmail(Event event) {
-        // Check events for today and send notification emails
+        kafkaConsumerService.processEvent(event);
+    }
+
+    // Schedule the email sending task 5 day before
+    @Scheduled(cron = "0 0 0 5 * ?")
+    public void sendScheduledEmailBeforeFiveDays(Event event) {
+        kafkaConsumerService.processEvent(event);
+    }
+
+    // Schedule the email sending task 10 days before
+    @Scheduled(cron = "0 0 0 10 * ?")
+    public void sendScheduledEmailBeforeTenDays(Event event) {
         kafkaConsumerService.processEvent(event);
     }
 
